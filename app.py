42409b614d2f76282dc71e69e6091949d76b00d3
Monisha Hitang <130748910+moni2code@users.noreply.github.com>
2023-07-27 10:40:36 +0200

    Test ability to push

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get('/')
+def get_info():
+    return {"message": "You have to enter the details of a house in json format"}
+
+#@app.post('/predict')
+#def prediction(input_data : HouseDetail)
